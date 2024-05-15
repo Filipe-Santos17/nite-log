01e18a2456e8ab4dab71ce34f862ef5b334dea9c
Lucca Rey <104277321+Luccamvmrey@users.noreply.github.com>
2024-05-15 20:00:24 -0300

    first commit

---
+++ src/presentation/user-auth/components/LoginPage.js
@@ -0,0 +1,13 @@
+import FilledButton from "../shared/FilledButton";
+
+const LoginPage = () => {
+    return (
+        <form>
+            <input type="text" placeholder="E-mail" />
+            <input type="password" placeholder="Senha" />
+            <FilledButton>Entrar</FilledButton>
+        </form>
+    );
+};
+
+export default LoginPage;
---
+++ src/presentation/user-auth/components/SignupPage.js
@@ -0,0 +1,14 @@
+import FilledButton from "../shared/FilledButton";
+
+const SignupPage = () => {
+    return (
+        <form>
+            <input type="text" placeholder="Nome de Usuário"/>
+            <input type="text" placeholder="E-mail"/>
+            <input type="text" placeholder="Senha"/>
+            <FilledButton>Registrar</FilledButton>
+        </form>
+    );
+};
+
+export default SignupPage;
